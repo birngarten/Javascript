0b5f55181b53d0f96a90e9c7e4f508040df094ac
birngarten <resitozyurt@gmail.com>
2021-05-24 22:37:37 +0200

    Ornekler

--- bolum4_dom/ornek1_selector/script.js
+++ bolum4_dom/ornek1_selector/script.js
@@ -1,5 +1,5 @@
 //======================================================
-//        getElementById()
+//                getElementById()
 //=====================================================
 
 const paragraf = document.getElementById("par");
@@ -14,3 +14,56 @@ buton.style.backgroundColor = "black";
 buton.style.color = "yellow";
 buton.style.fontSize = "18px";
 buton.innerHTML = "ARA";
+
+//======================================================
+//                getElementByTagName()
+//=====================================================
+
+const resim = document.getElementsByTagName("img");
+resim[0].style.width = "300px";
+resim[0].style.height = "300px";
+resim[1].style.border = " 3px solid red";
+
+//======================================================
+//                 getElementByClassName()
+//======================================================
+
+const h1 = document.getElementsByClassName("h1");
+h1[0].style.color = "red";
+h1[0].style.textAlign = "center";
+
+//======================================================
+//                  querySelector()
+//======================================================
+
+const baslik = document.querySelector(".title");
+baslik.innerHTML = "DOM Selector ✒";
+
+const body = document.querySelector("#body");
+body.style.backgroundImage = "linear-gradient(orange, red)";
+
+//======================================================
+//                  EVENTS
+//======================================================
+//  ORNEK-1. Mosue Over, Mouse Out
+//------------------------------------------------------
+
+document.querySelector(".h1").onmouseover = function () {
+  const h1 = document.querySelector(".h1");
+  h1.style.color = "white";
+  h1.style.backgroundColor = "green";
+};
+
+document.querySelector(".h1").onmouseout = function () {
+  const h1 = document.querySelector(".h1");
+  h1.style.color = "green";
+  h1.style.backgroundColor = "white";
+};
+
+// addEventListener()
+document.querySelector("#btn").addEventListener("mouseover", function () {
+  document.querySelector("#btn").style.width = "150px";
+});
+document.querySelector("#btn").addEventListener("mouseout", () => {
+  document.querySelector("#btn").style.width = "100px";
+});
