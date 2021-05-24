9b171624fa035a439db02b87b7e57121d8c17e38
birngarten <resitozyurt@gmail.com>
2021-05-24 20:23:48 +0200

    dom_selector

---
+++ bolum4_dom/ornek1_selector/script.js
@@ -0,0 +1,16 @@
+//======================================================
+//        getElementById()
+//=====================================================
+
+const paragraf = document.getElementById("par");
+paragraf.style.backgroundColor = "orange";
+paragraf.style.color = "white";
+paragraf.style.fontSize = "20px";
+
+const buton = document.getElementById("btn");
+buton.style.width = "100px";
+buton.style.height = "30px";
+buton.style.backgroundColor = "black";
+buton.style.color = "yellow";
+buton.style.fontSize = "18px";
+buton.innerHTML = "ARA";
