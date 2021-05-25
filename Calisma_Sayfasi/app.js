aea4ccc1a905bce26fa20f7890b5653824253f0f
birngarten <resitozyurt@gmail.com>
2021-05-25 21:16:16 +0200

    d-flex

--- Calisma_Sayfasi/app.js
+++
@@ -1,6 +0,0 @@
-function abc(ad, tarih) {
-  const yas = 2021 - tarih;
-  return console.log(`Benim adim ${ad} ve ${yas} yasindayim.`);
-}
-
-abc("Ali", 1979);
--- bolum4_dom/ornek3_domManipule/app.js
+++ bolum4_dom/ornek3_domManipule/app.js
@@ -6,10 +6,43 @@ document.querySelector(".off").onclick = function () {
   document.querySelector(".resim").src = "./img/lamba_off.gif";
 };
 
-// SORU : Ampulun uzerine mouse ile gelindiginde yansin, mouse ayrildiginda sonsun
-document.querySelector(".resim").onmouseover = function () {
-  document.querySelector(".resim").src = "./img/lamba_on.gif";
+// * SORU : Ampulun uzerine mouse ile gelindiginde yansin, mouse ayrildiginda sonsun
+// ? 1.YOL
+// document.querySelector(".resim").onmouseover = function () {
+//   document.querySelector(".resim").src = "./img/lamba_on.gif";
+// };
+// document.querySelector(".resim").onmouseout = function () {
+//   document.querySelector(".resim").src = "./img/lamba_off.gif";
+// };
+// ? 2.YOL
+document.querySelector(".resim").addEventListener("mouseover", () => {
+  document.querySelector(".resim").src = "img/lamba_on.gif";
+});
+document.querySelector(".resim").addEventListener("mouseout", function () {
+  document.querySelector(".resim").src = "img/lamba_off.gif";
+});
+//========================================================================
+// * checkbox'a tiklandiginda girilen text buyuk harf olsun
+// * Klavyeden her bir tusa basildiginda bu fonksiyon tetiklenecek.
+document.querySelector(".textbox").onkeyup = function () {
+  const checkbox = document.querySelector(".checkbox");
+  const textbox = document.querySelector(".textbox");
+
+  if (checkbox.checked == true) {
+    textbox.value = textbox.value.toUpperCase();
+  } else {
+    textbox.value = textbox.value.toLowerCase();
+  }
 };
-document.querySelector(".resim").onmouseout = function () {
-  document.querySelector(".resim").src = "./img/lamba_off.gif";
-};
+//==========================================================================
+
+// * index.html'de input-div'in altina yeni bir h1 elemani olusturalim
+// * sanki orada h1 elamani varmis gibi islev gorur
+const sonDiv = document.querySelector(".input-div");
+const h1 = document.createElement("h1");
+const yazi = document.createTextNode("Programlama Dilleri");
+h1.appendChild(yazi);
+sonDiv.after(h1); // ? index.html'deki input-div'in sonrasina h1'i ekliyoruz
+h1.style.marginTop = "20px";
+h1.style.color = "red";
+h1.style.textAlign = "left";
