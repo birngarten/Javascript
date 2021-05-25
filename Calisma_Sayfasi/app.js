91691bfdf14030fd527240acd1f1ef3ea7bb174b
birngarten <resitozyurt@gmail.com>
2021-05-25 10:07:05 +0200

    licht on-off

--- Calisma_Sayfasi/app.js
+++ Calisma_Sayfasi/app.js
@@ -1,13 +1,6 @@
-function namel(a, b) {
-  return console.log(a * b);
+function abc(ad, tarih) {
+  const yas = 2021 - tarih;
+  return console.log(`Benim adim ${ad} ve ${yas} yasindayim.`);
 }
 
-namel(2, 3);
-
-const carp = function name(c, d) {
-  return console.log(c * d);
-};
-carp(3, 5);
-
-const carpma = (e, f) => e * f;
-console.log(carpma(4, 6));
+abc("Ali", 1979);
--- bolum4_dom/ornek3_domManipule/app.js
+++ bolum4_dom/ornek3_domManipule/app.js
@@ -2,7 +2,14 @@
 document.querySelector(".on").onclick = function () {
   document.querySelector(".resim").src = "./img/lamba_on.gif";
 };
-
 document.querySelector(".off").onclick = function () {
+  document.querySelector(".resim").src = "./img/lamba_off.gif";
+};
+
+// SORU : Ampulun uzerine mouse ile gelindiginde yansin, mouse ayrildiginda sonsun
+document.querySelector(".resim").onmouseover = function () {
+  document.querySelector(".resim").src = "./img/lamba_on.gif";
+};
+document.querySelector(".resim").onmouseout = function () {
   document.querySelector(".resim").src = "./img/lamba_off.gif";
 };
