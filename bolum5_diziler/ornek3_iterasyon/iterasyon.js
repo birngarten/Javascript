329aef72654e70a91ff0a477798d871143bbf843
birngarten <resitozyurt@gmail.com>
2021-05-29 22:07:29 +0200

    javascript map

--- bolum5_diziler/ornek3_iterasyon/iterasyon.js
+++ bolum5_diziler/ornek3_iterasyon/iterasyon.js
@@ -1,19 +1,121 @@
+// ======================================================
+//            DİZİLERDE İTERASYON
+// ======================================================
 const koordinatlar = [120, 100, -100, 220, 330];
 let negatif = 0;
 let pozitif = 0;
-
-//* FOR DONGUSU
+// ÖRNEK: FOR DÖNGÜLERİN DİZİLER İLE KULLANIIM
 // for (let i = 0; i < koordinatlar.length; i++) {
 //   koordinatlar[i] < 0 ? negatif++ : pozitif++;
 // }
-// console.log(
-//   `Negatif Koordinat Sayisi: ${negatif} Pozitif Koordinat Sayisi: ${pozitif}`
-// );
-
-//* FOR IN DONGUSU
+// FOR IN DONGUSU
 for (let i in koordinatlar) {
   koordinatlar[i] < 0 ? negatif++ : pozitif++;
 }
 console.log(
-  `Negatif Koordinat Sayisi: ${negatif} Pozitif Koordinat Sayisi: ${pozitif}`
+  `Negatif Koordinat Sayısı: ${negatif} Pozitif Koordinat sayisi ${pozitif}`
 );
+//---------------------------------------------------------------
+// SORU: Bir hayvanat bahçesinde bulunan hayvan türlerinin bir dizide
+// saklandığı varsayalım. Bu hayvanları türüne göre aramamızı sağlayacak
+// ve o türden kaç adet bulunduğu ana programa döndürecek fonksiyonu
+// yazınız. Eğer o türden bir hayvan yok ise bulunamadığını yazdırsın.
+//----------------------------------------------------------------
+const hayvanlar = ["fil", "deve", "kuş", "deve", "fare", "kedi", "deve"];
+const ara = prompt("aramak istenilen hayvan türünü giriniz:");
+const bulDondur = function (ara) {
+  let hayvanSayisi = 0;
+  for (let i in hayvanlar) {
+    if (hayvanlar[i] == ara.toLowerCase()) {
+      hayvanSayisi++;
+    }
+  }
+  return hayvanSayisi;
+};
+const sayi = bulDondur(ara);
+sayi == 0
+  ? console.log("Aradiginiz hayvan bulunamadi")
+  : console.log(`Aradiginiz ${ara} 'den ${sayi} adet bulunmaktadir`);
+
+//  =========   FOR OF DONGUSU ===========
+// for of dongusu for in dongusunun bir cok veri yapisini kapsayacak sekilde guncellenmis halidir. Bu dongude dizi icersindeki veriye erisirken indisleme kullanmaya gerek yoktur.
+let arabalar = ["BMW", "Volvo", "Mini"];
+let yazı = "";
+for (let i of arabalar) {
+  console.log(i);
+  yazı += i + " "; // i:  dizinin i. elemani demektir.
+}
+console.log(yazı);
+
+// ============== FOREACH METHODU ==============================
+let ogrenciler = ["Jhon", "Ali", "Can"];
+ogrenciler.forEach((x) => console.log(x)); // 1.Yontem =>
+
+// function yazdir(a) {
+//   console.log(a);
+// }
+// ogrenciler.forEach(yazdir); // 2.Yontem (function ile)
+
+//-----------------------------------------------------------------
+//
+// Toplama Ornegi (UI'da (Ekranda) gozuksun)
+const fiyatlar = [200, 300, 125, 450, 333, 123];
+let t = 0;
+fiyatlar.forEach((f) => (t += f));
+console.log(t);
+document.querySelector(".toplam").innerHTML = `${t} EU`; // bu atamayla toplami consle'a degil EKRANa yazdiriyoruz
+
+//-----------------------------------------------------------------
+// Alternatif Yontem ile toplama
+toplam = 0;
+fiyatlar.forEach((a, i, d) => {
+  // a= iterasyon i=indis d=dizi(array)
+  toplam += a;
+  console.log(`${i}. iterasyon: ${toplam}`);
+});
+console.log(t);
+document.querySelector(".toplam").innerHTML = `${t} EU`;
+//--------------------------------------------------------------
+// SORU : fiyatlar dizisinde her bir fiyata %10 zam yapalim.
+// const fiyatlar = [200, 300, 125, 450, 333, 123];
+let toplamFiyat = 0;
+let zamliFiyatlar = [];
+fiyatlar.forEach((deger, indis) => {
+  zamliFiyatlar[indis] = (deger * 1.1).toFixed(2);
+  toplamFiyat += +zamliFiyatlar[indis];
+}); // toFixed(2): virgulden sonra iki basamak olsun
+console.log(zamliFiyatlar);
+console.log(toplamFiyat);
+//-------------------------------------------------------------
+
+// ============== MAP METHODU ==============================
+// SORU : Dizinin herbir elemaninin iki katini aliniz
+const rakamlar = [3, 5, 3, 2, 6, 7, 9];
+const ikiKat = rakamlar.map((x) => x * 2);
+console.log(ikiKat);
+//-------------------------------------------------------------
+// SORU : Isimler dizisindeki herbir ismi buyuk harf olarak yeni bir diziye saklayalim
+const isimler = ["Ahmet", "Mehmet", "Saffet", "ismet"];
+// map() metodu normal sartlarda bir dizi dondurur
+const buyuk = isimler.map((isim) => isim.toUpperCase());
+buyuk.forEach((isim) => console.log(isim));
+// map() metodundan sonra eger bir terminal islemi(forEach gibi) kullanilirsa map() metodu bir dizi dondurmez.
+//Pipeline
+isimler.map((isim) => isim.toUpperCase()).forEach((isim) => console.log(isim));
+
+//--------------------------------------------------------------
+// SORU : Dolar , Euro parite hesaplama
+tlFiyatlar = [120, 340, 550, 245, 322.5, 789];
+const dolarKur = document.querySelector(".dolar");
+const euroKur = document.querySelector(".euro");
+
+const dolar = document.querySelector(".dolar-fiyat");
+const euro = document.querySelector(".euro-fiyat");
+
+dolarKur.onchange = function () {
+  dolar.innerHTML = tlFiyatlar.map((tl) => Math.round(tl / dolarKur.value));
+};
+
+euroKur.onchange = function () {
+  euro.innerHTML = tlFiyatlar.map((tl) => Math.round(tl / euroKur.value));
+};
